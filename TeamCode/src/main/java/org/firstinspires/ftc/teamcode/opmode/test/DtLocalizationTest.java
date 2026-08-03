213f4597d7cf0e6c219ec0cc00f43e7824a59e2d
Aayush Sahoo <sahoo.aayush@gmail.com>
2026-08-03 13:16:49 -0400

    Localization test to make sure the drivetrain framework works
    Localization test to make sure the drivetrain framework works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/DtLocalizationTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.opmode.BaseOpMode;
+
+@TeleOp(name = "Drivetrain Localization Test", group = "Tests")
+public class DtLocalizationTest extends BaseOpMode {
+    @Override
+    protected void onLoop() {
+        telemetry.addData("Pose", robot.drivetrain.follower.getPose());
+        telemetry.update();
+    }
+}
